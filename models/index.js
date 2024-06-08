4d53f2e0cd5da6864a0e56a1304a15877865e8ed
Aaron Ho <bicecreamaar@gmail.com>
2024-06-07 23:10:10 -0700

    added post route for categories

--- models/index.js
+++ models/index.js
@@ -12,19 +12,26 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id'
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  foreignKey: 'product_id',
+  as: 'tags'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id'
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  foreignKey: 'tag_id',
+  as: 'products'
 });
 
 module.exports = {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,25 +3,50 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const data = await Category.findAll({
+      include: [{
+        model: Product,
+        as: 'products',
+      }],
+    });
+    res.status(200).json(data);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const data = await Category.findByPk( req.params.id, {
+      include: [{
+        model: Product,
+        as: 'products',
+      }],
+    });
+    console.log(data);
+    res.status(200).json(data);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const data = await Category.create(req.body);
+  res.status(200).json(data);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
